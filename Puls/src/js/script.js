22756bf745f4387974736971463c6839596fcd23
Bauyrzhan <kinbau91@gmail.com>
2022-06-01 18:18:21 +0600

    work to media

--- Puls/src/js/script.js
+++ Puls/src/js/script.js
@@ -5,20 +5,18 @@ $(document).ready(function(){
         prevArrow: '<button type="button" class="slick-prev"><img src="../icons/prev_icons/prev.png"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/prev_icons/next.png"></button>',
         responsive: [
-       /*    {
-            breakpoint: 1024,
+          {
+            breakpoint: 768,
             settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
+              dots: true,
+              arrows: false
             }
-          }, */
+          }, 
           {
             breakpoint: 600,
             settings: {
               slidesToShow: 5,
-              slidesToScroll: 1
+              slidesToScroll: 3
             }
           },
           {
