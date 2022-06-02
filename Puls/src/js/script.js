187554b29b179458c491e0768c1ffdaf6f838386
Bauyrzhan <kinbau91@gmail.com>
2022-06-02 18:26:55 +0600

    tinyslidemode

--- Puls/src/js/script.js
+++ Puls/src/js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
@@ -28,4 +28,20 @@ $(document).ready(function(){
           }
         ]
       });
+      }); */
+
+      const slider = tns({
+        container: '.carousel__inner',
+        items: 1,
+        slideBy: 'page',
+        autoplay: false,
+        controls: false,
+        nav: false
       });
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+  slider.goTo('next');
+});
