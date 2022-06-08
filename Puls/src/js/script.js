73a65dd479c0c7a8860fdbf6d4685c6341537298
Bauyrzhan <kinbau91@gmail.com>
2022-06-08 19:17:48 +0600

    tabs

--- Puls/src/js/script.js
+++ Puls/src/js/script.js
@@ -45,3 +45,25 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
   slider.goTo('next');
 });
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+  $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});   
+
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+  $(this)
+    .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+    .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+});
+
+  $('.catalog-item__link').each(function(i) {
+      $(this).on('click', function(e) {
+          e.preventDefault();
+          $('.catalog-item__content').toggleClass(catalog-item__content_active);
+          $('.catalog-item__list').toggleClass('catalog-item__list_active');
+    });
+});
+
+// кнопка tabs
