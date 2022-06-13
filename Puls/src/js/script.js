aed79f6bee772016aa33cd370336e94ad02ae78e
Bauyrzhan <kinbau91@gmail.com>
2022-06-13 09:17:48 +0600

    modal option

--- Puls/src/js/script.js
+++ Puls/src/js/script.js
@@ -34,9 +34,9 @@ $(document).ready(function(){
     container: '.carousel__inner',
     items: 1,
     slideBy: 'page',
-    autoplay: false,
     controls: false,
-    nav: false
+    nav: false,
+    autoplay: false
 });
   document.querySelector('.prev').addEventListener('click', function () {
     slider.goTo('prev');
@@ -65,3 +65,56 @@ function toggleSlide(item) {
 
 toggleSlide('.catalog-item__link');
 toggleSlide('.catalog-item__back');
+
+// Modal
+
+  $('[data-modal=consultation]').on('click', function() { 
+      $('.overlay, #consultation').fadeIn('slow');
+});
+
+$('[data-modal=consultation]').on('click', function() {
+  $('.overlay, #consultation').fadeIn('slow');
+});
+$('.modal__close').on('click', function() {
+  $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+});
+
+$('.button_mini').each(function(i) {
+  $(this).on('click', function() {
+      $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+      $('.overlay, #order').fadeIn('slow');
+  });
+});
+
+function validateForms(form){
+  $(form).validate({
+      rules: {
+          name: {
+              required: true,
+              minlength: 2
+          },
+          phone: "required",
+          email: {
+              required: true,
+              email: true
+          }
+      },
+      messages: {
+          name: {
+              required: "Пожалуйста, введите свое имя",
+              minlength: jQuery.validator.format("Введите {0} символа!")
+            },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свою почту",
+            email: "Неправильно введен адрес почты"
+          }
+      }
+  });
+}
+
+validateForms('#consultation-form');
+validateForms('#consultation form');
+validateForms('#order form');
+
+$('input[name=phone]').mask("+7 (999) 999-99-99");
